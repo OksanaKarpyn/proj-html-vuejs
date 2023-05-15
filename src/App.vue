6a85627b47ce315f7c4e1e9c8c725eaf0cfdc1db
Oksana Karpyn <oksana101992@gmail.com>
2023-05-15 17:35:25 +0200

    agg dark mode

--- src/App.vue
+++ src/App.vue
@@ -5,29 +5,82 @@ import MainComp from './components/MainComp.vue';
 
 export default {
   name: 'AppVue',
-  data() {
-    return {
-      store,
-    }
-  },
   components: {
     HeaderComp,
     MainComp,
+  },
+  data() {
+    return {
+      store,
+      darkMode: false
+    }
+  },
+  methods: {
+    toggle() {
+      this.darkMode = !this.darkMode
+    }
   }
+
 }
 
 </script>
 
 <template>
-  <header>
-    <HeaderComp></HeaderComp>
-  </header>
-  <main>
-    <MainComp></MainComp>
-  </main>
-  <footer></footer>
+  <div class="wrapper">
+    <button class="btn  btn-outline-ligth dark" @click="toggle"
+      :class="this.darkMode == true ? 'orange' : 'lilla'">click</button>
+    <div class="content" :class="{ 'dark-mode': darkMode }">
+      <header>
+        <HeaderComp></HeaderComp>
+      </header>
+      <main>
+        <MainComp></MainComp>
+      </main>
+      <footer></footer>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
 @use '../src/style/main.scss';
+
+.wrapper {
+  position: relative;
+
+  .dark {
+    z-index: 2;
+    position: absolute;
+    top: 20px;
+    right: 10px;
+  }
+
+  .lilla {
+    background-color: #B6B7D2;
+    color: #fff
+  }
+
+  .orange {
+    background-color: #FB9300;
+
+  }
+
+  .dark-mode {
+    background-color: #1f1f1f;
+    color: coral;
+
+    li {
+      color: coral;
+
+
+    }
+
+    select {
+      color: coral;
+
+      option {
+        color: #000;
+      }
+    }
+  }
+}
 </style>
